66739016c2df1b500a99e36b4203ed9075a755de
Carlos Gámez <hardpx@gmail.com>
2022-08-12 11:26:20 -0500

    project setup

--- server.js
+++ server.js
@@ -2,4 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, )
+app.get('/pedals', (req,res) => {
+    res.send('GET /pedals response');
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+});
